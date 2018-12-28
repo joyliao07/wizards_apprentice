5e15eb3c4e52e5c1ac7229d840c99c630d898a03
Hannah Sindorf <hannah@hsindorf.com>
2018-12-28 13:48:43 -0800

    finish up styles

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,18 +1,16 @@
-var inputs = document.querySelectorAll( '.upload' );
-Array.prototype.forEach.call( inputs, function( input )
-{
-	var label	 = input.nextElementSibling.firstChild,
-		labelVal = label.innerHTML;
+const inputs = document.querySelectorAll('.upload');
+Array.prototype.forEach.call(inputs, function(input) {
+	let label = input.nextElementSibling.firstChild
+	let labelVal = label.innerHTML;
 
-	input.addEventListener( 'change', function( e )
-	{
-		var fileName = '';
-		if( this.files && this.files.length > 1 )
-			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+	input.addEventListener( 'change', function(event) {
+		let fileName = '';
+		if (this.files && this.files.length > 1)
+			fileName = (this.getAttribute( 'data-multiple-caption') || '').replace('{count}', this.files.length);
 		else
-			fileName = e.target.value.split( '\\' ).pop();
+			fileName = event.target.value.split('\\').pop();
 
-		if( fileName )
+		if (fileName)
 			label.innerHTML = '<i class="fas fa-upload"></i> ' + fileName;
 		else
 			label.innerHTML = '<i class="fas fa-upload"></i> ' + labelVal;
