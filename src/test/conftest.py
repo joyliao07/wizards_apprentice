23c2c47b757f73d7ae42daaa903c22be777d50d2
Scott Currie <vitriolik@gmail.com>
2019-01-02 12:02:11 -0800

    Added test fixtures

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,22 +1,37 @@
 from ..gvision import ProcessedImage
+import os
 import pytest
+
+
+base_path = os.path.split(__file__)[0]
 
 
 @pytest.fixture
 def green_car():
-    return ProcessedImage('../../uploads/green_car.jpg')
+    print(__file__)
+    return ProcessedImage(os.path.join(base_path, 'test_images/green_car.jpg'))
 
 
 @pytest.fixture
 def red_chair():
-    return ProcessedImage('../../uploads/red_chair.jpg')
+    return ProcessedImage(os.path.join(base_path, 'test_images/red_chair.jpg'))
 
 
 @pytest.fixture
 def black_laptop():
-    return ProcessedImage('../../uploads/laptop.jpg')
+    return ProcessedImage(os.path.join(base_path, 'test_images/laptop.jpg'))
 
 
 @pytest.fixture
 def red_teapot():
-    return ProcessedImage('../../uploads/red_teapot.jpg')
+    return ProcessedImage(os.path.join(base_path, 'test_images/red_teapot.jpg'))
+
+
+@pytest.fixture
+def blue_bird():
+    return ProcessedImage(os.path.join(base_path, 'test_images/blue_bird.jpg'))
+
+
+@pytest.fixture
+def green_sofa():
+    return ProcessedImage(os.path.join(base_path, 'test_images/green_sofa.jpg'))
