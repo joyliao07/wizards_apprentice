5749ca1744eae1ce521da9e454d84e3d5e275cd3
Joyce Liao <joyliao07@gmail.com>
2019-01-02 12:11:31 -0800

    change description UTC to PST

--- src/routes.py
+++ src/routes.py
@@ -170,7 +170,7 @@ def history():
         prompt = Prompt.query.filter(Prompt.id == what.prompt_id).first()
 
         history.append({
-            'time': str(what.submission_time)[:16] + ' UTC',
+            'time': str(what.submission_time)[:16] + ' PST',
             'image': what.image_path,
             'adjective': prompt.adjective,
             'noun': prompt.noun,
@@ -198,7 +198,7 @@ def players():
         score = Submission.query.filter(Submission.submitted_by == what.submitted_by).filter(Submission.passes_prompt == True).count()
 
         compiled.append({
-            'time': str(what.submission_time)[:16] + ' UTC',
+            'time': str(what.submission_time)[:16] + ' PST',
             'user': user.username,
             'score': score,
             'image': what.image_path,
